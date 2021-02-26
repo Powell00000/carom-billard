f9bd9bf392818b1c51eae0c12b67a6dae9cd50e1
Paweł Miłaszewicz <pawelm@kool2play.com>
2021-02-26 10:37:12 +0100

    namespace for ball

---
+++ Assets/Code/Gameplay/Ball.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace Assets.Code.Gameplay
+{
+    public class Ball : MonoBehaviour
+    {
+        private float radius;
+
+        public virtual void Initialize()
+        {
+            CalculateRadius();
+        }
+
+        protected virtual void FixedUpdate()
+        {
+            StickToTable();
+        }
+
+        private void CalculateRadius()
+        {
+            radius = transform.localScale.y / 2;
+        }
+
+        private void StickToTable()
+        {
+
+            if (Physics.Raycast(transform.position, Vector3.down, out var raycastHit, LayerMask.GetMask("Table")))
+            {
+                transform.position = transform.position.WithY(raycastHit.point.y + radius);
+            }
+        }
+    }
+}
