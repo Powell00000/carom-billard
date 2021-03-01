394194d0f394a1d5a8743c96f5d3fe866f91c1bc
Paweł Miłaszewicz <pawelm@kool2play.com>
2021-03-01 10:11:25 +0100

    single responsiobility controllers

---
+++ Assets/Code/Gameplay/HitBallController.cs
@@ -0,0 +1,62 @@
+﻿using Assets.Code.Saveable;
+using System;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Code.Gameplay
+{
+    internal class HitBallController : MonoBehaviour, IInitializable, IRevertable, IDisposable
+    {
+        [Inject] private GameplayController gameplayCtrl;
+        [Inject] private CameraController cameraCtrl;
+        [Inject] private InputController inputCtrl;
+
+        [SerializeField] private Ball playingBall;
+
+        private SaveableStruct<Vector3> savedForce;
+
+        public Action BallHit;
+
+        public void Initialize()
+        {
+            inputCtrl.OnLeftButtonClick += HitBall;
+        }
+
+        public void Revert()
+        {
+            if (savedForce == null)
+            {
+                return;
+            }
+
+            playingBall.AddVelocity(savedForce.SavedValue);
+        }
+
+        public void Store()
+        {
+            //nothing
+        }
+
+        private void Update()
+        {
+            if (gameplayCtrl.CurrentState != GameplayController.GameState.Playing)
+            {
+                return;
+            }
+
+            playingBall.DrawExtrapolatedLine(cameraCtrl.LookDirection);
+        }
+
+        private void HitBall()
+        {
+            savedForce = new SaveableStruct<Vector3>(cameraCtrl.LookDirection * 8);
+            BallHit?.Invoke();
+            playingBall.AddVelocity(savedForce.SavedValue);
+        }
+
+        public void Dispose()
+        {
+            inputCtrl.OnLeftButtonClick -= HitBall;
+        }
+    }
+}
--- Assets/Code/Gameplay/Installers/GameplayInstaller.cs
+++ Assets/Code/Gameplay/Installers/GameplayInstaller.cs
@@ -10,12 +10,17 @@ namespace Assets.Code.Gameplay.Installers
 {
     internal class GameplayInstaller : MonoInstaller
     {
-        [SerializeField]
-        private GameplayController gameplayCtrl;
+        [SerializeField] private GameplayController gameplayCtrl;
+        [SerializeField] private CameraController cameraCtrl;
+        [SerializeField] private HitBallController hitBallCtrl;
 
         public override void InstallBindings()
         {
             Container.BindInterfacesAndSelfTo<GameplayController>().FromInstance(gameplayCtrl);
+            Container.BindInterfacesAndSelfTo<CameraController>().FromInstance(cameraCtrl);
+            Container.BindInterfacesAndSelfTo<HitBallController>().FromInstance(hitBallCtrl);
+
+            Container.BindInterfacesAndSelfTo<InputController>().AsSingle();
         }
     }
 }
